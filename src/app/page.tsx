21c393f91735f8dfd5406a8d367a21be3c6c9424
vanie0000 <124703685+vanie0000@users.noreply.github.com>
2025-10-05 02:59:26 +0530

    added about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import HeroSection from "@/components/hero-section"
+import ContentSection from "@/components/about"
 export default function Page(){
   return(
     <div>
       <HeroSection/>
+      <ContentSection/>
     </div>
   )
 }
---
+++ src/components/about.tsx
@@ -0,0 +1,23 @@
+export default function ContentSection() {
+    return (
+      <section className="py-16 md:py-32">
+        <div className="mx-auto max-w-5xl px-6">
+          <div className="grid gap-6 md:grid-cols-2 md:gap-12">
+            <h2 className="text-4xl font-medium">
+              hi there, i am shraavani, a student of mathematics and computing
+              at nsut delhi.
+            </h2>
+            <div className="space-y-6">
+              <p>
+                my core work is built upon the T3 stack principles, that leverages
+                next.js and typescript for end-to-end type safety, complemented
+                by tailwind css for rapid, responsive ui development.
+              </p>
+              <p>when i'm not writing code, you can usually find me listening to music or watching tv shows (guess who's a breaking bad fanatic).</p>
+              <p>i'm always open to discussing new opportunities or collaborations. feel free to explore my projects, or get in touch with me!</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+}
